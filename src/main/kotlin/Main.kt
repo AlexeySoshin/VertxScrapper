b4b0901f7fcfec957062e0f6788c895383bcea60
Alexey Soshin <alexey.soshin@gettaxi.com>
2017-12-03 11:17:38 +0100

    Extract initialization to a separate method

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -17,29 +17,16 @@ fun main(args: Array<String>) {
 
     val vertx = Vertx.vertx()
 
-    val deploymentOptions = DeploymentOptions().setWorker(true)
-
+    // Register codecs to be able to send custom objects over EventBus
     vertx.eventBus().registerCodec(DocumentCodec()).registerCodec(ImageCodec())
 
-    vertx.deployVerticle(ContentWriter("../manga/"),
-            deploymentOptions)
-    vertx.deployVerticle("ContentFetcher",
-            deploymentOptions.setInstances(2))
-    vertx.deployVerticle(AttributeFinder("#vungdoc img", "src"),
-            deploymentOptions.setInstances(1))
-    vertx.deployVerticle("HTMLParser",
-            deploymentOptions.setInstances(2))
-    vertx.deployVerticle("HTMLFetcher",
-            deploymentOptions.setInstances(4), { _ ->
+    deployVerticles(vertx)
 
-        println("Open UI by using this link: http://localhost:8080/")
-        // One Piece manga is notoriously long, 886 chapters
-        repeat(886, { i ->
-            vertx.eventBus().send(HTMLFetcher.CONSUMES,
-                    "http://manganeli.com/chapter/read_one_piece_manga_online_free4/chapter_${i + 1}")
-        })
-    })
+    serveStats(vertx)
+}
 
+private fun serveStats(vertx: Vertx) {
+    // Gather some statistics about the progress
     val writtenBytes = AtomicLong(0)
     val writtenFiles = AtomicInteger(0)
 
@@ -53,6 +40,7 @@ fun main(args: Array<String>) {
 
     val router = Router.router(vertx)
 
+    // Also provide statistics on http://localhost:8080/stats as JSON
     router.get("/stats").handler { req ->
         req.response().end(statsAsJson(writtenBytes, writtenFiles))
     }
@@ -62,10 +50,39 @@ fun main(args: Array<String>) {
     vertx.createHttpServer().requestHandler(router::accept).listen(8080)
 }
 
+private fun deployVerticles(vertx: Vertx) {
+
+    val deploymentOptions = DeploymentOptions().setWorker(true)
+
+    vertx.deployVerticle(ContentWriter("../manga/"),
+            deploymentOptions)
+    vertx.deployVerticle("ContentFetcher",
+            deploymentOptions.setInstances(2))
+    vertx.deployVerticle(AttributeFinder("#vungdoc img", "src"),
+            deploymentOptions.setInstances(1))
+    vertx.deployVerticle("HTMLParser",
+            deploymentOptions.setInstances(2))
+
+    // Wait until first verticle in the pipeline is deployed to start sending the data
+    vertx.deployVerticle("HTMLFetcher",
+            deploymentOptions.setInstances(4), { _ ->
+
+        println("Open UI by using this link: http://localhost:8080/")
+        // One Piece manga is notoriously long, 886 chapters
+        repeat(886, { i ->
+            vertx.eventBus().send(HTMLFetcher.CONSUMES,
+                    "http://manganeli.com/chapter/read_one_piece_manga_online_free4/chapter_${i + 1}")
+        })
+    })
+}
+
 fun statsAsJson(writtenBytes: AtomicLong, writtenFiles: AtomicInteger): String {
     return JsonObject(Stats(writtenBytes.toLong(), writtenFiles.toInt()).toMap()).toString()
 }
 
+/**
+ * Create SockJS handler to be able to send data to clients over WebSockets
+ */
 fun sockJsHandler(vertx: Vertx): Handler<RoutingContext>? {
     return SockJSHandler.create(vertx).bridge(BridgeOptions().addOutboundPermitted(PermittedOptions().setAddressRegex("stats")))
 }
